0fdf359abccfde5e8e52c88a3f4fdd5f637f08e3
GuillN <gnaassan@gmail.com>
2020-02-08 15:00:44 +0100

    videos loop

--- src/components/home/carouselPane.js
+++ src/components/home/carouselPane.js
@@ -3,11 +3,23 @@ import ReactPlayer from "react-player";
 
 class CarouselPane extends React.Component {
 
+    handleProgress = () => {
+        let duration = this.player.getDuration();
+        let currentTime = this.player.getCurrentTime();
+        if (duration - currentTime < 1) {
+            this.player.seekTo(1)
+        }
+    };
+
+    ref = player => {
+        this.player = player
+    };
+
     render() {
         const url = `https://www.facebook.com/emgalai/videos/${this.props.videoId}/`;
         return (
             <div className="carousel-pane">
-                <ReactPlayer url={url} playing muted loop/>
+                <ReactPlayer ref={this.ref} url={url} playing muted onProgress={this.handleProgress}/>
             </div>
         )
     }
