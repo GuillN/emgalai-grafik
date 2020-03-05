c36df4400336454fd6b970e254deaa5cb7be9f41
GuillN <gnaassan@gmail.com>
2020-03-05 12:49:36 +0100

    fixed dependencies

--- src/components/print/print.js
+++ src/components/print/print.js
@@ -1,17 +1,12 @@
-import React, {useEffect} from "react"
+import React from "react"
 import Nav from "../nav/nav";
 import Footer from "../footer/footer";
 
 const Print = () => {
-
-    useEffect(() => {
-
-    });
-
     return(
         <div style={{backgroundColor: 'black', height: '100vh'}}>
             <Nav print={true}/>
-            Print page
+
             <Footer/>
         </div>
     )
