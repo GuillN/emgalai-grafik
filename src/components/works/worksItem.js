1cf88a7ec163d0802f4c795a90bef0ab76be1982
GuillN <gnaassan@gmail.com>
2020-03-21 14:42:56 +0100

    wait for img load

--- src/components/works/worksItem.js
+++ src/components/works/worksItem.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {MobileView, BrowserView} from "react-device-detect"
 import LazyLoad from 'react-lazy-load'
 
 const WorksItem = props => {
+    const [load, setLoad] = useState("loading");
+
+    const handleLoaded = () => {
+        setLoad("loaded")
+    };
+
+    const handleFailed = () => {
+        setLoad("failed")
+    };
+
     return(
         <div>
             <BrowserView>
                 <div className="portfolio-item">
-                    <LazyLoad>
-                        <img className="portfolio-item-image" src={props.img} alt={props.alt}/>
-                    </LazyLoad>
+                    {load === "loading" ?
+                        <div>Loading</div> : ""}
+                        <LazyLoad>
+                            <img onLoad={handleLoaded} onError={handleFailed} className="portfolio-item-image"
+                                 src={props.img} alt={props.alt}/>
+                        </LazyLoad>
                     <p className="portfolio-item-text">{props.text}</p>
                 </div>
             </BrowserView>
             <MobileView>
                 <div className="portfolio-item">
                     <LazyLoad>
-                        <img className="portfolio-item-image-mobile" src={props.img} alt={props.alt}/>
+                        <img onLoad={handleLoaded} onError={handleFailed} className="portfolio-item-image-mobile"
+                             src={props.img} alt={props.alt}/>
                     </LazyLoad>
                     <p className="portfolio-item-text">{props.text}</p>
                 </div>
