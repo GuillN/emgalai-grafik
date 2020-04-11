79e4e41085f8d774f6cc9f41e49a53dc2c9fce32
GuillN <gnaassan@gmail.com>
2020-04-11 14:39:25 +0200

    SSR

--- src/ServerApp.js
+++ src/ServerApp.js
@@ -2,7 +2,7 @@ import {StaticRouter} from 'react-router';
 import Routes from "./Routes";
 import React from 'react'
 
-export default props => {
+export default function Root(props) {
     return (
         <StaticRouter location={props.url}>
             <Routes/>
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import ReactDOMServer from 'react-dom/server'
 import App from "./App";
 import React from "react";
+import Root from "./ServerApp";
 
 const express = require('express');
 const port = process.env.PORT || 8080;
@@ -10,7 +11,7 @@ const app = express();
 
 app.get('*', (req, res) => {
     const html = ReactDOMServer.renderToString(
-        <App url={req.url}/>
+        <Root url={req.url}/>
     )
     res.send(renderFullPage(html))
 });
